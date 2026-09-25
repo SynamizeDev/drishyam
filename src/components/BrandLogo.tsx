0e7ec674625083c931d2ff6b7f838fabb6f9210e
Gourav Bairagi <bairagig157@gmail.com>
2026-09-25 12:48:03 +0530

    UI fixes on logo

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import brandLogo from "../../public/assets/brand-logo.webp";
+import brandLogo from "../../public/assets/brand-logo.png";
 
 export interface BrandLogoProps {
   variant?: "full";
